3608d4dd72b5365654a100680dc45af88f8e7e52
ShubhamGupta24 <guptashubhamofficial24@gmail.com>
2025-06-07 21:11:30 +0530

    removed the error

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    Plugins: [
+    plugins: [
       'expo-router/babel',
       'react-native-reanimated/plugin', // Ensure this is at the end of the plugins array
     ],
